13dd4e13956e366af82032ce825d3f68d044d43e
Chester Holtz <chesterholtz@gmail.com>
2015-11-29 01:43:08 -0500

    html directory include project proposal

--- mine_users.py
+++ mine_users.py
@@ -3,10 +3,10 @@ import requests, json, sys
 github_url = "https://api.about.me/api/v2/json/users/view/random"
 key = "?client_id=cb12c2533a888bc5cc3f7dd5d0cb473a2c221f5d"
 num_calls = "&profile_number=" + str(sys.argv[1])
-data_file = open('data', 'w')
+data_file = open('data.txt', 'w')
 
 try :
-    r = requests.post(github_url+key+num_calls)
+    r = requests.post(github_url+key+num_calls+"&extended=true")
 except requests.exceptions.RequestException as e:
     print e
     sys.exit(1)
